28f8c59c0b46d12b4bc5a940dcda061d62be4dea
JonathonMoore1 <30606521+JonathonMoore1@users.noreply.github.com>
2018-01-12 18:25:34 -0600

    Basic HTML layout and Firebase established

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBVguDcHq3s8ImsqD4HEfi_q0jMqnw5fZQ",
+    authDomain: "train-scheduler-72648.firebaseapp.com",
+    databaseURL: "https://train-scheduler-72648.firebaseio.com",
+    projectId: "train-scheduler-72648",
+    storageBucket: "train-scheduler-72648.appspot.com",
+    messagingSenderId: "733284910073"
+};
+
+firebase.initializeApp(config);
